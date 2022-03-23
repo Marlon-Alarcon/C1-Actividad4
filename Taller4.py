41268c0194359176b09c93024b795ed411f85632
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-03-23 18:32:41 -0500

    agregar persona

--- Taller4.py
+++ Taller4.py
@@ -1,49 +1,65 @@
+import os
 
-def menu():
-    men = 0
-    em = empleado()
-    while men != 4:
-        print("")
-        print("  ╔════════════════════════╗")
-        print("  ║    ----  MENÚ  ----    ║")
-        print("  ╚════════════════════════╝")
-        print("  ║ 1. AGREGAR EMPLEADO    ║")
-        print("  ║ 2. MOSTRAR EMPLEADO    ║")
-        print("  ║ 3. ELIMINAR EMPLEADO   ║")
-        print("  ║ 4. Salir               ║")
-        print("  ╚════════════════════════╝")
-        print("")
-        men = int(input(" Ingrese una opcion por favor "))
+class Persona:
 
-        if men == 1:
-            print("")
-            limpiar()
-        
-        elif men == 2:
-            print("")
-            em.mostrarEmpleado()
-            limpiar()
-           
+    def __init__(self):
+        self.personas = {}
 
-        elif men == 3:
-            print("Opcion 3")
-            em.eliminarEmpleado()
-
-            
-
-        elif men == 4:
-            print("")
-            print(" -----   Has salido   -----")
-            print(" ***** Muchas Gracias *****")
-        
+    def agregarPersonas(self):
+        self.id = input("ID: ")
+        if self.id in self.personas:
+            print("Este codigo ya existe.")
         else:
-            print("")
-            print(" -----  Opcion no valida  -----")
-            print(" ----- Intente Nuevamente -----")
-            print("")
-            input(" -- Presione una tecla para continuar")
-            print("")
-            limpiar()
+            self.cedula = input("Ingrese su cedula: ")
+            self.nombre = input("Ingrese su nombre: ")
+            self.apellido = input("Ingrese su apellido: ")
+            self.direccion = input("Ingrese su direccion: ")
+            self.telefono = input("Ingrese su telefono: ")
+            self.personas[self.id] = (self.cedula, self.nombre, self.apellido, self.direccion, self.telefono)
+
+    def menu (self, opciones):
+        while(True):
+            os.system("clear")
+            for item in range (len(opciones)):
+                print (opciones[item])
+
+            opcion = input("Digite una opcion correcta: ")
+
+            if opcion == "1":
+                os.system("clear")
+                self.agregarEmpleado()
+
+            elif opcion == "2":
+                os.system("clear")
+                self.mostrarEmpleado()
+                input("Digite enter para continuar")
+            
+            elif opcion == "3":
+                break
+
+            elif opcion == "4":
+                print("")
+                print(" -----   Has salido   -----")
+                print(" ***** Muchas Gracias *****")
+                break
+def menuPrincipal():
+    os.system("clear")
+    opciones = ( #menu en forma de tupla
+        " ╔════════════════════════╗",
+        " ║    ----  MENÚ  ----    ║",
+        " ╚════════════════════════╝",
+        " ║ 1. AGREGAR EMPLEADO    ║",
+        " ║ 2. MOSTRAR EMPLEADO    ║",
+        " ║ 3. ELIMINAR EMPLEADO   ║",
+        " ║ 4. Salir               ║",
+        " ╚════════════════════════╝"
+    )
+    emp = Empleado ()
+    emp.menu(opciones)
+
+def main():
+    menuPrincipal()
+
 
 if __name__ == "__main__":
-    menu()
+    main()
