12c38c6e4f9dc663e60a08f17290a9eb9cb6df63
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2022-03-23 18:34:13 -0500

    Agregar empleado

--- Taller4.py
+++ Taller4.py
@@ -17,7 +17,34 @@ class Persona:
             self.telefono = input("Ingrese su telefono: ")
             self.personas[self.id] = (self.cedula, self.nombre, self.apellido, self.direccion, self.telefono)
 
+class Empleado(Persona):
+    def __init__(self):
+        super().__init__()
 
+    def agregarEmpleado(self):
+        super().agregarPersonas()
+        self.__salario = float(input("Ingrese su Salario: "))
+        self.alimentacion = 0
+        self.transporte = 0
+
+        if (self.__salario<2000000):
+            self.alimentacion = 80000
+            self.transporte = 60000
+        
+        self.pension = (self.__salario * 0.04)
+        self.salud = (self.__salario * 3.375)/100
+
+        self.devengado = self.alimentacion + self.transporte + self.__salario
+
+        self.deduccion = (self.__salario - self.salud - self.pension)
+        self.personas[self.id] = (self.cedula, self.nombre, self.apellido, self.direccion, self.telefono, self.__salario, self.alimentacion, self.transporte, self.pension, self.salud, self.devengado, self.deduccion)
+
+    def mostrarEmpleado(self):
+        if len(self.personas) == 0:
+            print("La lista está vacia")
+        else:
+            for i, v in self.personas.items():
+                print(f"{i}: {v}")
 
     def menu (self, opciones):
         while(True):
